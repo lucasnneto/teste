4b8bf8a4463efa1b6ef39668a87643a73645fea1
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-09-09 16:29:31 -0300

    ws remove

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var app = require("express")();
 var http = require("http").Server(app);
 const porta = process.env.PORT || 3000;
-const server = new WebSocket.Server({ server: http });
 app.get("/", (req, res) => {
   res.send(new Date());
 });
